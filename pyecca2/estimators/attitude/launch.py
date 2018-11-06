1cff2a378d1760e7c2b29aa975ff8a3ed843ec30
James Goppert <james.goppert@gmail.com>
2018-11-06 17:59:35 -0500

    Clean launch file.

--- pyecca2/estimators/attitude/launch.py
+++ pyecca2/estimators/attitude/launch.py
@@ -20,42 +20,35 @@ default_params = {
     'params': {}
 }
 
+eqs = algorithms.eqs()
 
-def launch_sim(params):
+
+def init_params(params):
     p = dict(default_params)
     for k, v in params.items():
         if k not in p.keys():
             raise KeyError(k)
         p[k] = v
+    return p
 
-    print('x0', p['x0'])
 
-    eqs = algorithms.eqs()
+def launch_sim(params):
+    p = init_params(params)
     core = uros.Core()
     Simulator(core, eqs, p['x0'])
-
     for name in p['estimators']:
         AttitudeEstimator(core, name, eqs[name], p['initialize'])
-
     logger = uros.Logger(core)
-
     core.init_params()
     for k, v in p['params'].items():
         core.set_param(k, v)
-
     core.run(until=p['tf'])
     print(p['name'], 'done')
     return logger.get_log_as_array()
 
 
 def launch_monte_carlo_sim(params):
-    p = dict(default_params)
-
-    for k, v in params.items():
-        if k not in p.keys():
-            raise KeyError(k)
-        p[k] = v
-
+    p = init_params(params)
     if p['n_monte_carlo'] == 1:
         d = dict(p)
         d.pop('n_monte_carlo')
@@ -73,27 +66,15 @@ def launch_monte_carlo_sim(params):
 
 
 def launch_replay(params):
-    p = dict(default_params)
-
-    for k, v in params.items():
-        if k not in p.keys():
-            raise KeyError(k)
-        p[k] = v
-
-    eqs = algorithms.eqs()
+    p = init_params(params)
     core = uros.Core()
-
     replay.ULogReplay(core, p['replay_log_file'])
-
     for name in p['estimators']:
         AttitudeEstimator(core, name, eqs[name], p['initialize'])
-
     logger = uros.Logger(core)
-
     core.init_params()
     for k, v in p['params'].items():
         core.set_param(k, v)
-
     core.run(until=p['tf'])
     print(p['name'], 'done')
     return logger.get_log_as_array()
