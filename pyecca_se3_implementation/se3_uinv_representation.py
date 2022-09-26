70dbeac331898837d2163fcb7d1da8df629556ca
wsribunm <worawissribunma@gmail.com>
2022-09-25 22:52:23 -0400

    Notes on mrp and quaterions

--- pyecca_se3_implementation/se3_uinv_representation.py
+++ pyecca_se3_implementation/se3_uinv_representation.py
@@ -5,6 +5,7 @@ from pyecca.lie.so3 import Quat, Dcm, Euler, Mrp
 from pyecca.lie.r3 import R3
 from pyecca.lie.se3 import SE3
 from pyecca.test import test_lie
+import numpy as np
 
 
 '''
@@ -14,7 +15,11 @@ This initialize the matrix representation for se3 lie algebra elements
 v = ca.DM([0.1, 0.2, 0.3]) #translational component
 R = Dcm.from_euler(ca.DM([0.1, 0.2, 0.3])) #Rotational component using Dcm
 
-test_lie
+R = np.array(ca.DM(R))
+print(f"{v} and R is {R}")
+# lie_elements = ca.horzcat(np.transpose(v),R)
+
+
 '''
 lie_alg_se3= ca.SX(4, 4)
 lie_alg_se3[0, 1] = -R[2]
