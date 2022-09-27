567296bccf784dc6f2f3cb5f9d5ae1129143ec11
wsribunm <worawissribunma@gmail.com>
2022-09-26 21:19:45 -0400

    Finished adjoint matrix

--- pyecca_se3_implementation/se3_uinv_representation.py
+++ pyecca_se3_implementation/se3_uinv_representation.py
@@ -9,26 +9,55 @@ import numpy as np
 
 
 '''
-This initialize the matrix representation for se3 lie algebra elements
+This code intends to perform U_inv matrix of se3
 '''
-  #erms = ca.SX([0.1, 0.2, 0.3, 4, 5, 6]) # terms : [theta1, theta2, theta3, x,y,z]
-v = ca.DM([0.1, 0.2, 0.3]) #translational component
-R = Dcm.from_euler(ca.DM([0.1, 0.2, 0.3])) #Rotational component using Dcm
 
-R = np.array(ca.DM(R))
-print(f"{v} and R is {R}")
-# lie_elements = ca.horzcat(np.transpose(v),R)
+class SE3_diff_corr:
+  group_shape = (6, 6)
+  group_params = 12
+  algebra_params = 6
 
+# coefficients 
+  x = ca.SX.sym('x')
+  C1 = ca.Function('a', [x], [ca.if_else(ca.fabs(x) < eps, 1 - x ** 2 / 6 + x ** 4 / 120, ca.sin(x)/x)])
+  C2 = ca.Function('b', [x], [ca.if_else(ca.fabs(x) < eps, 0.5 - x ** 2 / 24 + x ** 4 / 720, (1 - ca.cos(x)) / x ** 2)])
+  del x
 
-'''
-lie_alg_se3= ca.SX(4, 4)
-lie_alg_se3[0, 1] = -R[2]
-lie_alg_se3[0, 2] = R[1]
-lie_alg_se3[1, 0] = R[2]
-lie_alg_se3[1, 2] = R[0]
-lie_alg_se3[2, 0] = -R[1]
-lie_alg_se3[2, 1] = R[0]
-lie_alg_se3[0, 4] = v[0]
-lie_alg_se3[1, 4] = v[1]
-lie_alg_se3[2, 4] = v[2]
-'''
+  def __init__(self, SO3=None):
+    if SO3 == None:
+      self.SO3 = so3.Dcm()
+
+  @classmethod
+  def check_group_shape(cls, a):
+    assert a.shape == cls.group_shape or a.shape == (cls.group_shape[0],)
+
+  @classmethod
+  def adjoint_se3(cls, v): #input vee operator
+    ad_se3 = ca.SX(6, 6)
+    ad_se3[0,1] = -v[5,0]
+    ad_se3[0,2] = v[3,0]
+    ad_se3[0,4] = -v[2,0]
+    ad_se3[0,5] = v[1,0]
+    ad_se3[1,0] = v[5,0]
+    ad_se3[1,2] = -v[3,0]
+    ad_se3[1,3] = v[2,0]
+    ad_se3[1,5] = -v[0,0]  
+    ad_se3[2,0] = -v[4,0]
+    ad_se3[2,1] = v[3,0]
+    ad_se3[2,3] = -v[1,0]
+    ad_se3[2,4] = v[0,0]
+    ad_se3[3,4] = -v[5,0]
+    ad_se3[3,5] = v[4,0]
+    ad_se3[4,3] = v[5,0]
+    ad_se3[4,5] = -v[3,0]
+    ad_se3[5,3] = -v[4,0]
+    ad_se3[5,4] = v[3,0]
+    return ad_se3
+
+  @classmethod #Does these inputs using vee operator work? Seems like there should be a better way
+  def se3_diff_correction_inv(cls, v, C1,C2): #input vee operator
+    u_inv = ca.SX(6, 6)
+    #u_inv[0,0] = C2*(v[3,0]**2 - v[])
+    
+    #is it possible to combine theta1, theta2, theta3 into theta?
+
