b72e2a7d21c11b9c7b9717186c9d6572b9d05114
James Goppert <james.goppert@gmail.com>
2023-01-26 10:58:38 -0500

    Move pictures out of notebook.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- pyecca/casadi_graph_fix.py
+++ pyecca/casadi_graph_fix.py
@@ -1,18 +1,25 @@
 from IPython.display import Image, display
 import os
+from pathlib import Path
 import tempfile
+import casadi as ca
 from casadi import print_operator as print_operator_orig
 import casadi.tools.graph as mod_graph
+import pydot
 
 
-def dotdraw(pdot, width=1920, direction="LR"):
-    curdir = os.getcwd()
+def draw_casadi(expr: ca.SX or ca.MX, filename: str=None, width: int=1920, direction: str="LR") -> pydot.Graph:
+    curdir = Path(os.path.abspath(os.getcwd()))
     # dot draw creates a source.dot file, lets move to the tmp directory
     os.chdir(tempfile.gettempdir())
-    graph = mod_graph.dotgraph(pdot, direction=direction)
-    png = graph.create_png()
-    os.chdir(curdir)
-    return Image(png, width=width)
+    graph = mod_graph.dotgraph(expr, direction=direction)  # pydot.Dot
+    if filename is None:
+        png = graph.create_png()
+        os.chdir(curdir)
+        return Image(png, width=width)
+    else:
+        os.chdir(curdir)
+        return graph.write_png(filename)
 
 
 def print_operator_escaped(expr, strs):
