d46c2ce7c5b86560878ba6a4b3aaa9d3c0b6c14d
James Goppert <james.goppert@gmail.com>
2023-01-31 12:52:49 -0500

    Remove docker.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- docker/install/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-source ~/.profile
-source ~/.bashrc
-
-bash -l -c "$@"
--- docker/install/user_setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# setup .profile, note bashrc doesn't get sourced by docker by defualt, .profile does
-echo "source ~/.venv/bin/activate" >> ~/.profile
